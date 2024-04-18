6af2cdf4c723048ce30bc5336e9ffdabbd27e4fc
atarico <atarico@gmail.com>
2024-04-18 15:53:55 -0300

    clase de ciclos

---
+++ ciclos.cpp
@@ -0,0 +1,22 @@
+int valorIngresado = 10;
+
+
+for (int i = 0; i <= valorIngresado; i++){
+
+    if( i % 2 == 0 ){
+
+        cout << i << endl;
+
+    }
+}
+
+
+
+/*
+    5
+    10
+    15
+    20
+    25
+    [...]
+*/
